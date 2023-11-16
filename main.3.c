306c590fb1338976c5e8946a2a5fdf886d1dd239
thddlrla <okjh0999@naver.com>
2023-11-16 16:16:15 +0900

    commit 11

---
+++ main.3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void swap(int *x, int *y){
+	int temp;
+    temp =*x;
+    *x=*y;
+    *y=temp;  //다 별만 붙이기  
+}
+int main(int argc, char *argv[]) {
+	
+	int a=3;
+	int b=5;
+	swap(&a,&b); // 변수의 주소값으로  
+	printf("a:%i, b:%i\n",a,b);
+	
+	
+	
+	return 0;
+}
